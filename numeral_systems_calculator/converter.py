91f0fff3767f79c57496c25aa23aef614873c8e3
Uladzislau <vladtett2000@gmail.com>
2023-07-19 20:44:07 +0200

    init: add solution file

---
+++ numeral_systems_calculator/converter.py
@@ -0,0 +1,9 @@
+
+
+def numeral_converter(number: int, base: int = 10) -> int:
+    #  write your solution here
+    pass
+
+if __name__ == '__main__':
+    number, base = int(input()), int(input())
+    print(numeral_converter(number, base))
