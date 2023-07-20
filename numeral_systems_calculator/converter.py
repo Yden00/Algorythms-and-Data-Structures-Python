4d27ebdeab334181b6d4c6db1f44a70d0622afe3
D27N <Triznuk.N@mail.ru>
2023-07-20 11:34:57 +0300

    ne krasavchik ?(

--- numeral_systems_calculator/converter.py
+++ numeral_systems_calculator/converter.py
@@ -1,8 +1,9 @@
 
 
 def numeral_converter(number: int, base: int = 10) -> int:
-    #  write your solution here
-    pass
+    dickt = {8: oct(number), 2: bin(number), 16: hex(number).upper(), 10: 'aa' + str(number)}
+    return dickt[base][2:] if number >= 0 else int('asd')
+
 
 if __name__ == '__main__':
     number, base = int(input()), int(input())
