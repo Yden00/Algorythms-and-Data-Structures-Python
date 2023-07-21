7c5d2f3a105905be71e9addf3b1ff30e93db9176
Uladzislau <vladtett2000@gmail.com>
2023-07-21 13:53:10 +0200

    fix: change return type

--- numeral_systems_calculator/converter.py
+++ numeral_systems_calculator/converter.py
@@ -1,6 +1,6 @@
 
 
-def numeral_converter(number: int, base: int = 10) -> int:
+def numeral_converter(number: int, base: int = 10) -> str:
     dickt = {8: oct(number), 2: bin(number), 16: hex(number).upper(), 10: 'aa' + str(number)}
     return dickt[base][2:] if number >= 0 else int('asd')
 
